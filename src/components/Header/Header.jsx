7a293d59b76eec71e7862a98f8ba65cf58eee2c7
jacqi-f <jfong3878@gmail.com>
2024-02-06 14:20:37 -0800

    added Header imports

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "../Header/Header.scss";
+// import Logo from "../../../assets/logo/InStock-Logo_1x.png";
+
+const Header = () => {
+  return (
+    <>
+      <h1>work</h1>
+    </>
+  );
+};
+
+export default Header;
