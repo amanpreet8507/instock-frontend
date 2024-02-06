c80ec48c8692dbca218f70699c69fb70d4abee85
jacqi-f <jfong3878@gmail.com>
2024-02-06 14:20:24 -0800

    added Header imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.scss';
+import "./App.scss";
+import Header from "../src/components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       App
+      <Header />
     </div>
   );
 }
