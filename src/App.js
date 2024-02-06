e4493b3f1e503fa33b77903c39876864c16bd3fd
Blessy Pinto <blessyrosepinto@gmail.com>
2024-02-06 13:35:11 -0800

    client side router and pages folder commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WarehousesPage from "./pages/WarehousesPage";
+import InventoryPage from "./pages/InventoryPage";
+
+
 
 function App() {
   return (
     <div className="App">
-      App
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<WarehousesPage />}/>
+          <Route path="/warehouses" element={<WarehousesPage />}/>
+
+          <Route path="/inventory" element={<InventoryPage/>}/>
+          <Route path="/warehouses/:id" element={<InventoryPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
