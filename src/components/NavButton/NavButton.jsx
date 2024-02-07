fc01c8e3129de0d33b37e7981d0437cfa8076edb
jacqi-f <jfong3878@gmail.com>
2024-02-07 15:26:57 -0800

    updated header before peer review for push commit

--- src/components/NavButton/NavButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../NavButton/NavButton.scss";
-
-const NavButton = () => {
-  return <div className="header__nav--button">Warehouses</div>;
-};
-
-export default NavButton;
