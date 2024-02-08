68ad3849c242c1239f587d978e331c5fa7da41ea
jacqi-f <jfong3878@gmail.com>
2024-02-08 11:03:42 -0800

    added onClose functionality to DeleteWarehouse

--- src/components/DeleteWarehouse/DeleteWarehouse.jsx
+++ src/components/DeleteWarehouse/DeleteWarehouse.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import "./DeleteWarehouse.scss";
+import closeButton from "../../assets/icons/close-24px.svg";
 
-const DeleteWarehouse = () => {
+const DeleteWarehouse = ({ onDelete, onClose }) => {
   return (
-    <section>
-      <div>
-        <h1>Delete Television inventory item?</h1>
-      </div>
-      <div>
-        <p>
-          Please confirm that you'd like to delete Television from the inventory
-          list. You won't be able to undo this action.{" "}
-        </p>
-      </div>
-      <section className="button__box">
-        <div className="button__box--cancel">
-          <p>Cancel</p>
+    <section className="overlay">
+      <div className="overlay__modal">
+        <div onClick={onClose} className="overlay__close">
+          <img src={closeButton} />
         </div>
-        <div className="button__box--delete">
-          <p>Delete</p>
+        <div>
+          <h1>Delete Television inventory item?</h1>
         </div>
-      </section>
+        <div>
+          <p>
+            Please confirm that you'd like to delete Television from the
+            inventory list. You won't be able to undo this action.{" "}
+          </p>
+        </div>
+        <section className="button__box">
+          <div onClick={onClose} className="button__box--cancel">
+            <p>Cancel</p>
+          </div>
+          <div className="button__box--delete">
+            <p>Delete</p>
+          </div>
+        </section>
+      </div>
     </section>
   );
 };
--- src/components/WarehouseTable/WarehouseTable.jsx
+++ src/components/WarehouseTable/WarehouseTable.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './WarehouseTable.scss'; 
 import TableHeading from "../TableHeading/TableHeading";
 import WarehouseTableMobile from "../../components/WarehouseTable/WarehouseTableMobile";
 import rightIcon from "../../assets/icons/chevron_right-24px.svg";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg"
+import DeleteWarehouse from '../DeleteWarehouse/DeleteWarehouse';
 const WarehouseTable = () => {
+
+const [isModalOpen, setIsModalOpen] = useState(false)
+
+
+  
   return (
     <>
+    {isModalOpen && (<DeleteWarehouse onDelete={()=>{}} onClose={()=> setIsModalOpen(false)}/>)}
     <table className="table__warehouse">
     <tr className="table__row table__heading-row">
       <TableHeading heading="WAREHOUSE" />
@@ -21,7 +28,7 @@ const WarehouseTable = () => {
       <td className="table__data">#43, USA</td>
       <td className="table__data">Pamela Ajula</td>
       <td className="table__data">0-000-000-000<br/>abc@demo.com</td>
-      <td className="table__data table__warehouse__actions"><img src={deleteIcon} className='table__data__delete__icon' /><img src={editIcon} /></td>
+      <td className="table__data table__warehouse__actions"><img onClick={()=> setIsModalOpen(true)} src={deleteIcon} className='table__data__delete__icon' /><img src={editIcon} /></td>
     </tr>
   </table>
     <WarehouseTableMobile />
