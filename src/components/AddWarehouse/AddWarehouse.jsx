6eb48f9d051646223b8580ac4d3ddf453aa91f8b
jacqi-f <jfong3878@gmail.com>
2024-02-09 10:33:26 -0800

    added add to backend warehouse

--- src/components/AddWarehouse/AddWarehouse.jsx
+++ src/components/AddWarehouse/AddWarehouse.jsx
@@ -1,36 +1,160 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddWarehouse.scss";
-import TextField from "../TextField/TextField"
+import TextField from "../TextField/TextField";
 import backArrow from "../../assets/icons/arrow_back-24px.svg";
+import { api } from "../../axios/axios";
 
-const AddWarehouse = ({onCancel}) => {
+const phoneNumberRegex = /^\+\d{1,3}\s\(\d{3}\)\s\d{3}-\d{4}$/;
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+const AddWarehouse = ({ onCancel }) => {
+  const [warehouse_name, setWarehouseName] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [contact_name, setContactName] = useState("");
+  const [position, setPosition] = useState("");
+  const [phone_number, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [clickSubmit, setClickSubmit] = useState(false);
+
+  const handleSubmit = async () => {
+    setClickSubmit(true);
+
+    // validate the form
+    if (
+      warehouse_name === "" ||
+      address === "" ||
+      city === "" ||
+      country === "" ||
+      contact_name === "" ||
+      phone_number === "" ||
+      email === "" ||
+      !phoneNumberRegex.test(phone_number) ||
+      !emailRegex.test(email)
+    ) {
+      console.log("Form is invalid");
+      return;
+    }
+
+    try {
+      const res = await api.post("/warehouses", {
+        warehouse_name,
+        address,
+        city,
+        country,
+        contact_name,
+        contact_position: position,
+        contact_phone: phone_number,
+        contact_email: email,
+      });
+
+      if (res.status === 201) {
+        clearForm();
+        setClickSubmit(false);
+      }
+    } catch (error) {
+      console.log("🚀 ~ handleSubmit ~ error:", error);
+    }
+  };
+
+  const clearForm = () => {
+    setWarehouseName("");
+    setAddress("");
+    setCity("");
+    setCountry("");
+    setContactName("");
+    setPosition("");
+    setPhoneNumber("");
+    setEmail("");
+  };
+
   return (
     <div className="addWarehouse">
       <div className="addWarehouse__header">
-        <img src={backArrow} onClick={onCancel}/>
-        <p>Add New Warehouse</p>
+        <img src={backArrow} onClick={onCancel} />
+        <p className="addWarehouse__header--title">Add New Warehouse</p>
       </div>
+
       <div className="column addWarehouse__container">
         <h2>Warehouse Details</h2>
         <div className="column addWarehouse__textFields">
-            <TextField label="Warehouse Name"/>
-            <TextField label="Street Address"/>
-            <TextField label="City"/>
-            <TextField label="Country"/>
+          <TextField
+            label="Warehouse Name"
+            value={warehouse_name}
+            setValue={setWarehouseName}
+            error={
+              clickSubmit && warehouse_name === ""
+                ? "Warehouse name is required"
+                : ""
+            }
+          />
+          <TextField
+            label="Street Address"
+            value={address}
+            setValue={setAddress}
+            error={clickSubmit && address === "" ? "Address is required" : ""}
+          />
+          <TextField
+            label="City"
+            value={city}
+            setValue={setCity}
+            error={clickSubmit && city === "" ? "City is required" : ""}
+          />
+          <TextField
+            label="Country"
+            value={country}
+            setValue={setCountry}
+            error={clickSubmit && country === "" ? "Country is required" : ""} // if (clickSubmit && country === "") then return 'Country is required' else return "" (empty string
+          />
         </div>
       </div>
       <div className="column addWarehouse__container">
         <h2>Contact Details</h2>
         <div className="column addWarehouse__textFields">
-            <TextField label="Contact Name"/>
-            <TextField label="Position"/>
-            <TextField label="Phone Number"/>
-            <TextField label="Email"/>
+          <TextField
+            label="Contact Name"
+            value={contact_name}
+            setValue={setContactName}
+            error={
+              clickSubmit && contact_name === ""
+                ? "Contact name is required"
+                : ""
+            }
+          />
+          <TextField label="Position" value={position} setValue={setPosition} />
+          <TextField
+            label="Phone Number"
+            value={phone_number}
+            setValue={setPhoneNumber}
+            error={
+              clickSubmit &&
+              (phone_number === "" ||
+              !phoneNumberRegex.test(phone_number))
+                ? "Enter valid phone number"
+                : ""
+            }
+          />
+          <TextField
+            label="Email"
+            value={email}
+            setValue={setEmail}
+            error={
+              clickSubmit && (email === "" || !emailRegex.test(email))
+                ? "Enter valid email"
+                : ""
+            }
+          />
         </div>
       </div>
       <div className="addWarehouse__buttons">
-        <button onClick={onCancel} className="addWarehouse__cancelBtn">Cancel</button>
-        <button className="addWarehouse__addBtn">+Add Warehouse</button>
+        <button onClick={onCancel} className="addWarehouse__cancelBtn">
+          Cancel
+        </button>
+        <button className="addWarehouse__addBtn" onClick={handleSubmit}>
+          +Add Warehouse
+        </button>
       </div>
     </div>
   );
--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./TextField.scss";
 
-const TextField = ({label, value="", setData}) => {
+const TextField = ({label, value="", setValue, error = ""}) => {
   return <div className="textfield">
     <p className="textfield__label">{label}
     </p>
-    <input type="text" placeholder={label} value={value} />
+    <input type="text" placeholder={label} value={value} onChange={(e) => setValue(e.target.value)} />
+    {error !== "" && <p className="textfield__error">{error}</p>}
   </div>;
 };
 
