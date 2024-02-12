06fe3bd3153c2dee4ba61e4a739f76bbfabe8315
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-02-11 22:07:21 -0800

    functionality changes

--- src/components/FormComponents/FormDropdown.jsx
+++ src/components/FormComponents/FormDropdown.jsx
@@ -4,8 +4,8 @@ const FormDropdown = ({ label, value, options, onChange }) => {
       <label className="textfield__label">{label}</label>
       <select id="inventory" value={value} onChange={onChange}>
         <option className="textfield__option" value="">Select...</option>
-        {options.map((option) => (
-          <option className="textfield__option" key={option} value={option}>
+        {options.map((option, index) => (
+          <option className="textfield__option" key={index} value={option}>
             {option}
           </option>
         ))}
--- src/pages/AddEditInventoryPages/EditInventoryPage.jsx
+++ src/pages/AddEditInventoryPages/EditInventoryPage.jsx
@@ -1,31 +1,36 @@
+import React, { useState, useEffect } from "react";
 import MainHeader from "../../components/MainHeader/MainHeader";
+import "./EditInventoryPage.scss";
 import TextField from "../../components/FormComponents/TextField/TextField";
 import FormTextarea from "../../components/FormComponents/FormTextarea/FormTextarea";
-import "./EditInventoryPage.scss";
 import FormDropdown from "../../components/FormComponents/FormDropdown";
 import RadioButtonSelection from "../../components/FormComponents/RadioButtonSelection/RadioButtonSelection";
 import AddButton from "../../components/Buttons/AddButton";
 import CancelButton from "../../components/Buttons/CancelButton";
-import { useParams } from "react-router";
-import { useEffect, useState } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const EditInventoryPage = () => {
   const { id } = useParams();
-  const [inventoryData, setInventoryData] = useState([]);
+  const navigate = useNavigate();
+
+  const [inventoryData, setInventoryData] = useState({
+    item_name: "",
+    description: "",
+    category: "",
+    warehouse_name: "",
+    status: "",
+  });
   const [selectedStatus, setSelectedStatus] = useState("");
-  const [itemName, setItemName] = useState("");
-  const [description, setDescription] = useState("");
-  const [category, setCategory] = useState("");
-  const [warehouse, setWarehouse] = useState("");
+  const [clickSubmit, setClickSubmit] = useState(false);
 
   const categoryOptions = [
-      "Electronics",
-      "Apparel",
-      "Accessories",
-      "Health",
-      "Gear",
-      "Accessories",
+    "Electronics",
+    "Apparel",
+    "Accessories",
+    "Health",
+    "Gear",
+    "Accessories",
   ];
   const warehouseOptions = [
     "Brooklyn",
@@ -39,19 +44,54 @@ const EditInventoryPage = () => {
     "Chicago",
   ];
 
+  const handleSubmit = async () => {
+    setClickSubmit(true);
+
+    // Validate the form
+    if (
+      inventoryData.item_name === "" ||
+      inventoryData.description === "" ||
+      inventoryData.category === "" ||
+      inventoryData.warehouse_name === "" ||
+      inventoryData.status === ""
+    ) {
+      console.log("Form is invalid");
+      return;
+    }
+
+    try {
+      const res = await axios.put(`http://localhost:8080/inventories/${id}`, {
+        ...inventoryData,
+      });
+
+      if (res.status === 200) {
+        setInventoryData({
+          item_name: "",
+          description: "",
+          category: "",
+          warehouse_name: "",
+          status: "",
+        });
+        setSelectedStatus("");
+        setClickSubmit(false);
+        navigate("/inventory");
+      }
+    } catch (error) {
+      console.log("handleSubmit error:", error);
+    }
+  };
+
   const fetchData = async () => {
     try {
       const response = await axios.get(
         `http://localhost:8080/inventories/${id}`
       );
-      const inventoryData= response.data;
-      const { item_name, description, category, warehouse_name, status } = inventoryData;
-      setInventoryData(inventoryData);
-      setItemName(item_name);
-      setDescription(description);
-      setCategory(category);
-      setWarehouse(warehouse_name);
-      setSelectedStatus(status === "In Stock" ? "1" : "2");
+      const inventoryData = response.data;
+      setInventoryData({
+        ...inventoryData,
+        status: inventoryData.status === "In Stock" ? "1" : "2",
+      });
+      setSelectedStatus(inventoryData.status);
     } catch (error) {
       console.error(
         "Error while fetching inventory details in Edit Inventory Page",
@@ -59,62 +99,79 @@ const EditInventoryPage = () => {
       );
     }
   };
-
   useEffect(() => {
     fetchData();
   }, []);
 
-  const handleSubmit = async () => {
-    try {
-      await axios.put(`http://localhost:8080/inventories/${id}`, {
-        item_name: itemName,
-        description,
-        category,
-        warehouse_name: warehouse,
-        status: selectedStatus === "1" ? "In Stock" : "Out of Stock",
-      });
-      console.log("Inventory updated successfully!");
-    } catch (error) {
-      console.error("Error updating inventory:", error);
-    }
-  };
-
   return (
     <main>
+      <form onSubmit={handleSubmit}>
       <div className="main__header-div">
         <MainHeader headerTitle="Edit Inventory Item" />
       </div>
       <div className="main__body-container">
         <div className="main__body">
           <h2 className="main__h2">Item Details</h2>
-          <TextField label="Item Name" value={inventoryData.item_name} onChange={(e) => setItemName(e.target.value)}/>
-
-          <FormTextarea label="Description" value={inventoryData.description} onChange={(e) => setDescription(e.target.value)}/>
+          <TextField
+            label="Item Name"
+            value={inventoryData.item_name}
+            onChange={(e) =>
+              setInventoryData({ ...inventoryData, item_name: e.target.value })
+            }
+            error={clickSubmit && !inventoryData.item_name ? "Required" : ""}
+          />
+          <FormTextarea
+            label="Description"
+            value={inventoryData.description}
+            onChange={(e) =>
+              setInventoryData({
+                ...inventoryData,
+                description: e.target.value,
+              })
+            }
+            error={
+              clickSubmit && !inventoryData.description ? "Required" : ""
+            }
+          />
           <FormDropdown
             label="Category"
             value={inventoryData.category}
             options={categoryOptions}
-            onChange={(e) => setCategory(e.target.value)} 
+            onChange={(e) =>
+              setInventoryData({ ...inventoryData, category: e.target.value })
+            }
+            error={clickSubmit && !inventoryData.category ? "Required" : ""}
           />
         </div>
         <div className="main__body main__body-right">
           <h2 className="main__h2">Item Availability</h2>
           <RadioButtonSelection
             checked={selectedStatus}
-            onChange={(value) => setSelectedStatus(value)}
+            onChange={(value) => {
+              setInventoryData({ ...inventoryData, status: value });
+              setSelectedStatus(value);
+            }}
+            error={clickSubmit && !inventoryData.status ? "Required" : ""}
           />
           <FormDropdown
             label="Warehouse"
             value={inventoryData.warehouse_name}
             options={warehouseOptions}
-            onChange={(e) => setWarehouse(e.target.value)}
+            onChange={(e) =>
+              setInventoryData({
+                ...inventoryData,
+                warehouse_name: e.target.value,
+              })
+            }
+            error={clickSubmit && !inventoryData.warehouse_name ? "Required" : ""}
           />
         </div>
       </div>
       <div className="main__button-div">
         <CancelButton text="Cancel" className="main__button" />
-        <AddButton children="Save" className="main__button" onClick={handleSubmit} />
+        <AddButton children="Save" className="main__button" type="submit" />
       </div>
+      </form>
     </main>
   );
 };
